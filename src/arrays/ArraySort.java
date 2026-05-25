72e2c779c109d0893ea75402cb8028dfc0183ec2
NishanthiniB <nishasuresh1305@gmail.com>
2026-05-25 13:39:36 +0530

    Add Selection Sort

--- src/arrays/ArraySort.java
+++ src/arrays/ArraySort.java
@@ -5,20 +5,47 @@ import java.util.Arrays;
 public class ArraySort {
     static void main() {
        int[] array ={5,4,3,1,2};
-       int[] result =sortArr(array);
+//        int[] array = {1,2,3,4,5};
+       int[] result =selSort(array);
         System.out.println(Arrays.toString(result));
     }
+
+    //Bubble Sort
     static int[] sortArr(int[] array){
             int temp=0;
-       for(int i=0; i<array.length; i++){
-           for(int j=i+1; j<array.length; j++){
-               if(array[i]>array[j]){
-                  temp = array[i];
-                   array[i]=array[j];
-                   array[j]=temp;
+       for(int i=0; i<array.length; i++) {
+           boolean isSwapped = false;
+           for (int j = i + 1; j < array.length; j++) {
+               isSwapped = true;
+               if (array[i] > array[j]) {
+                   temp = array[i];
+                   array[i] = array[j];
+                   array[j] = temp;
                }
            }
+
+           if (!isSwapped) {
+               break;
+           }
+
        }
        return array;
+    }
+
+//Selection Sort
+    static int[] selSort(int[] array){
+        for(int i=0; i<array.length-1; i++){
+            int minIndex =i;
+            for(int j=i+1; j<array.length; j++){
+                if(array[j]<array[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp = array[i];
+            array[i]=array[minIndex];
+            array[minIndex]=temp;
+
+        }
+        return array;
     }
 }
