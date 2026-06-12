5cb296dabb305009ebdea07e0efa4900c9f7da2d
NishanthiniB <nishasuresh1305@gmail.com>
2026-06-12 06:49:40 +0530

    Search Rotated Array

---
+++ src/binarysearch/SearchRotatedArray.java
@@ -0,0 +1,40 @@
+package binarysearch;
+
+public class SearchRotatedArray {
+    static void main() {
+        int[] nums ={4,5,6,7,0,1,2};
+        int target=0;
+        System.out.println(search(nums,target));
+
+    }
+
+    public static int search(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+
+        while(low<=high){
+            int mid = low+(high-low) /2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low]&&target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+
+            else{
+                if(target<=nums[high]&&target>=nums[mid]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+
+            }
+        }
+        return -1;
+    }
+}
