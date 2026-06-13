0f567bb3380b26d7079cba068b18a0f1075c1756
NishanthiniB <nishasuresh1305@gmail.com>
2026-06-13 06:46:01 +0530

    Peak Element

--- src/binarysearch/PeakElement.java
+++ src/binarysearch/PeakElement.java
@@ -7,6 +7,8 @@ public class PeakElement {
         System.out.println(findPeakElement(nums));
     }
 
+
+    //using binary search
     public static int findPeakElement(int[] nums) {
         int left=0, right=nums.length-1;
 
@@ -21,5 +23,20 @@ public class PeakElement {
             }
         }
         return left;
+    }
+
+    //linear Search
+
+    static int findElement(int[] nums){
+        if (nums.length == 1) return 0;
+        if (nums[0] >= nums[1]) return 0;
+        if (nums[nums.length - 1] >= nums[nums.length - 2]) return nums.length - 1;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] >= nums[i - 1] && nums[i] >= nums[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
