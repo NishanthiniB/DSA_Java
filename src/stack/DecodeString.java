704a71bc0d002540b7ceed62917145b6f23e1c23
NishanthiniB <nishasuresh1305@gmail.com>
2026-06-17 09:22:46 +0530

    Decoding String using stack

---
+++ src/stack/DecodeString.java
@@ -0,0 +1,41 @@
+package stack;
+
+import java.util.Stack;
+
+public class DecodeString {
+
+    static void main() {
+        String str ="13[a]2[bc]";
+        System.out.println(decoding(str));
+    }
+
+    static String decoding(String s){
+
+        Stack<String> stack = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch!=']'){
+                stack.push(String.valueOf(ch));
+            }
+            else{
+                StringBuilder temp = new StringBuilder();
+                while(!stack.peek().equals("[")){
+                    temp.insert(0,stack.pop());
+                }
+                stack.pop();
+
+                StringBuilder num=new StringBuilder();
+                while(!stack.isEmpty() && Character.isDigit(stack.peek().charAt(0))){
+                    num.insert(0,stack.pop());
+                }
+
+                String repeated = temp.toString().repeat(Integer.parseInt(num.toString()));
+                stack.push(repeated);
+            }
+
+        }
+        StringBuilder result = new StringBuilder();
+        for(String str : stack) result.append(str);
+        return result.toString();
+
+    }
+}
