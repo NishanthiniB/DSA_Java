156a28330d1b43672bd2c72414adebdbc715cf5e
NishanthiniB <nishasuresh1305@gmail.com>
2026-06-24 05:43:33 +0530

    Add NSE & PGE

--- src/stack/NextGreaterElement.java
+++ src/stack/NextGreaterElement.java
@@ -9,8 +9,8 @@ import java.util.Stack;
 public class NextGreaterElement {
 
     static void main() {
-       int[] array = {4,6,8,1};
-        System.out.println(nextLargerElement(array));
+       int[] array = {10,9,8,7,6};
+        System.out.println(previousGreaterElement(array));
 
 
     }
@@ -20,6 +20,38 @@ public class NextGreaterElement {
           Stack<Integer> stack = new Stack<>();
         ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(arr.length,-1));
         for(int i=arr.length-1; i>=0; i--){
+            while(!stack.isEmpty()&&stack.peek()<=arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                result.set(i,stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+
+    }
+    public static ArrayList<Integer> nextSmallerElement(int[] arr) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(arr.length,-1));
+        for(int i=arr.length-1; i>=0; i--){
+            while(!stack.isEmpty()&&stack.peek()>=arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                result.set(i,stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+
+    }
+    public static ArrayList<Integer> previousGreaterElement(int[] arr) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(arr.length,-1));
+        for(int i=0; i<arr.length; i++){
             while(!stack.isEmpty()&&stack.peek()<=arr[i]){
                 stack.pop();
             }
